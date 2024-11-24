df1ee83889258f8c62532ce63c6ce3be9cd3d30b
Mohammad Faiz Abid <121609515+atfaizzz@users.noreply.github.com>
2024-11-25 02:38:27 +0530

    #binary indicator updated along with the number of time intervals

--- Load-Simulation/thermossysoreftes.m
+++ Load-Simulation/thermossysoreftes.m
@@ -1,18 +1,11 @@
 scenario_selection
 activities
-%Fortio thermanshs. An kai h hlektrikh thermansh den einai pleondhmofilhs
-%prepei na lhfthei ypopsh. Me to deikth "xeimonas" vlepe
-%"scenario_selection.m" orizoume th xrhsh h oxi analoga ton kairo.An den
-%xrhsimopoiountai ka8oloy bazoyme thn timh 0 sthn isxy. De vazoumeto
-%xeimonas=o dioti xrhsimopoieitai kai sta fvta kai sto aerothermo
 if xeimonas==1
     thermos1=zeros(1,96);
     thermos2=zeros(1,96);
-    %Epeidh oi thermosysoreftes leitourgoun me nyxterino revma orizoumethn ora
-    %diakophs kai enarkshs leitourgias tous
-    thermopower=2400;%isxys thermosyssoreftwn
-    tsys2=92; %vra enarkshs leitourgias
-    tsys1=20; %wra diakophs leitourgias
+    thermopower=2400;
+    tsys2=92;
+    tsys1=20;
     for f=1:1:p
         if f<=tsys1
             thermos1(f)=thermopower;
